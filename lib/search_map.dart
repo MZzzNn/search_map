f5e8882ef710c81da4f6b0f966da869af2a1eb6a
mazen <mazenn770@gmail.com>
2024-10-27 13:25:28 +0300

    :ambulance: Fixing dynamic style of search

--- lib/search_map.dart
+++ lib/search_map.dart
@@ -1,12 +1,12 @@
 library search_map;
 
+import 'dart:async';
 import 'dart:convert';
 import 'dart:developer';
 import 'package:easy_localization/easy_localization.dart';
-import 'package:uuid/uuid.dart';
-import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
 import 'package:dartz/dartz.dart' hide State;
 import 'package:http/http.dart' as http;
 
@@ -31,10 +31,14 @@ class SearchMap extends StatefulWidget {
   State<SearchMap> createState() => _SearchMapState();
 }
 
-class _SearchMapState extends State<SearchMap> {
+class _SearchMapState extends State<SearchMap> with SingleTickerProviderStateMixin {
   final GlobalKey _textFieldKey = GlobalKey();
   final TextEditingController _searchController = TextEditingController();
   late final ApiService _apiService;
+
+  late AnimationController _animationController;
+  late Animation<Offset> _slideAnimation;
+  late Animation<double> _fadeAnimation;
 
   List<PlaceDetails> _predictions = [];
   OverlayEntry? _overlayEntry;
@@ -44,13 +48,54 @@ class _SearchMapState extends State<SearchMap> {
   @override
   void initState() {
     super.initState();
-    _apiService = ApiService(widget.apiKey);
+    _initializeApiService();
+    _initializeAnimationController();
     _searchController.addListener(_onTextChanged);
   }
 
+  @override
+  void dispose() {
+    _disposeResources();
+    super.dispose();
+  }
+
+  void _initializeApiService() {
+    _apiService = ApiService(widget.apiKey);
+  }
+
+  void _initializeAnimationController() {
+    _animationController = AnimationController(
+      duration: const Duration(milliseconds: 300),
+      vsync: this,
+    );
+
+    _slideAnimation = Tween<Offset>(
+      begin: const Offset(0, -0.2),
+      end: Offset.zero,
+    ).animate(
+      CurvedAnimation(parent: _animationController, curve: Curves.easeOut),
+    );
+
+    _fadeAnimation = Tween<double>(
+      begin: 0.0,
+      end: 1.0,
+    ).animate(
+      CurvedAnimation(parent: _animationController, curve: Curves.easeIn),
+    );
+  }
+
+  void _disposeResources() {
+    _searchController.dispose();
+    _debounce?.cancel();
+    _animationController.dispose();
+  }
+
   void _onTextChanged() {
-    if (_showOverlay) _debounceSearch(_searchController.text);
-    if (_searchController.text.isEmpty) _clearPredictions();
+    if (_searchController.text.isEmpty) {
+      _hideOverlay();
+    } else {
+      _debounceSearch(_searchController.text);
+    }
   }
 
   void _debounceSearch(String query) {
@@ -80,14 +125,17 @@ class _SearchMapState extends State<SearchMap> {
           (error) => log('Error: $error'),
           (details) {
         widget.onClickAddress(details);
-        setState(() {
-          _searchController.text = prediction.description ?? '';
-          _clearPredictions();
-        });
-        _hideOverlay();
-        _resetOverlayVisibility();
+        _updateSearchField(prediction.description);
+        _hideOverlayWithDelay();
       },
     );
+  }
+
+  void _updateSearchField(String? text) {
+    setState(() {
+      _searchController.text = text ?? '';
+      _clearPredictions();
+    });
   }
 
   void _showPredictionOverlay() {
@@ -101,17 +149,14 @@ class _SearchMapState extends State<SearchMap> {
     _overlayEntry = null;
   }
 
-  void _resetOverlayVisibility() {
-    Future.delayed(const Duration(milliseconds: 300), () {
-      _showOverlay = true;
-    });
+  void _hideOverlayWithDelay() {
+    _animationController.reverse().whenComplete(_hideOverlay);
   }
 
   void _clearPredictions() {
     setState(() {
       _predictions.clear();
     });
-    _hideOverlay();
   }
 
   OverlayEntry _createOverlayEntry() {
@@ -119,87 +164,117 @@ class _SearchMapState extends State<SearchMap> {
     final size = renderBox.size;
     final offset = renderBox.localToGlobal(Offset.zero);
 
+    _animationController.forward();
+
     return OverlayEntry(
       builder: (context) => Positioned(
         left: offset.dx,
-        top: offset.dy + size.height,
-        width: size.width,
+        top: offset.dy + size.height + 10, // Offset by 10 pixels.
+        width: size.width, // Match width of text field.
         child: Material(
-          elevation: 4.0,
-          child: ListView.builder(
-            itemCount: _predictions.length,
-            shrinkWrap: true,
-            itemBuilder: (context, index) {
-              final prediction = _predictions[index];
-              return ListTile(
-                title: Text(prediction.description ?? ''),
-                onTap: () => _onPredictionSelected(prediction),
-              );
-            },
+          color: Colors.transparent,
+          child: FadeTransition(
+            opacity: _fadeAnimation,
+            child: SlideTransition(
+              position: _slideAnimation,
+              child: _buildPredictionList(),
+            ),
           ),
         ),
       ),
     );
   }
 
-  InputDecoration _defaultDecoration() {
-    return InputDecoration(
-      isCollapsed: true, // Ensures correct centering of text content
-      contentPadding: const EdgeInsets.symmetric(vertical: 12), // Center-align hint and label
-      border: InputBorder.none,
-      enabledBorder: InputBorder.none,
-      focusedBorder: InputBorder.none,
-      labelText: tr('search_address'),
-      hintText: tr('search_address'),
-      labelStyle: const TextStyle(color: Colors.black),
-      hintStyle: const TextStyle(color: Color(0xFFA3A3A3)),
-      alignLabelWithHint: true,
-      floatingLabelBehavior: FloatingLabelBehavior.never,
-      suffixIcon: AnimatedCrossFade(
-        duration: const Duration(milliseconds: 200),
-        firstChild: IconButton(
-          padding: EdgeInsets.zero,
-          icon: const Icon(
-            FontAwesomeIcons.xmark,
-            size: 20,
-            color: Color(0xFFA3A3A3),
-          ),
-          onPressed: () {
-            _searchController.clear();
-            widget.focusNode.unfocus();
-          },
-        ),
-        secondChild: const SizedBox(),
-        crossFadeState: _searchController.text.isNotEmpty
-            ? CrossFadeState.showFirst
-            : CrossFadeState.showSecond,
-      ),
-    );
-  }
-
-  InputDecoration _buildDecoration() {
-    return widget.decoration?.copyWith(
-      contentPadding: widget.decoration?.contentPadding ??
-          const EdgeInsets.symmetric(vertical: 12),
-    ) ??
-        _defaultDecoration();
-  }
-
-  @override
-  Widget build(BuildContext context) {
+  Widget _buildPredictionList() {
     return Container(
       decoration: BoxDecoration(
         color: Colors.white,
         borderRadius: BorderRadius.circular(10),
         boxShadow: [
           BoxShadow(
-            color: Colors.grey.withOpacity(0.25),
-            blurRadius: 5,
-            offset: const Offset(0, 2),
+            color: Colors.black.withOpacity(0.1),
+            blurRadius: 10,
+            offset: const Offset(0, 4),
           ),
         ],
       ),
-      padding: const EdgeInsets.symmetric(horizontal: 12,vertical: 5),
+      child: ListView.builder(
+        padding: const EdgeInsets.all(8.0),
+        itemCount: _predictions.length,
+        shrinkWrap: true,
+        itemBuilder: (context, index) {
+          final prediction = _predictions[index];
+          return ListTile(
+            title: Text(prediction.description ?? ''),
+            onTap: () => _onPredictionSelected(prediction),
+          );
+        },
+      ),
+    );
+  }
+
+  void _clearSearchField() {
+    _searchController.clear();
+    widget.focusNode.unfocus();
+    _hideOverlay();
+  }
+
+  InputDecoration _buildDecoration() {
+    return widget.decoration?.copyWith(
+      contentPadding: widget.decoration?.contentPadding ??
+          const EdgeInsets.symmetric(vertical: 12),
+      suffixIcon: IconButton(
+        padding: EdgeInsets.zero,
+        icon: const Icon(
+          FontAwesomeIcons.xmark,
+          size: 20,
+          color: Color(0xFFA3A3A3),
+        ),
+        onPressed: _clearSearchField,
+      ),
+    ) ??
+        _defaultDecoration();
+  }
+
+  InputDecoration _defaultDecoration() {
+    return InputDecoration(
+      isCollapsed: true,
+      contentPadding: const EdgeInsets.symmetric(vertical: 12),
+      border: InputBorder.none,
+      labelText: tr('search_address'),
+      hintText: tr('search_address'),
+      labelStyle: const TextStyle(color: Colors.black),
+      hintStyle: const TextStyle(color: Color(0xFFA3A3A3)),
+      alignLabelWithHint: true,
+      floatingLabelBehavior: FloatingLabelBehavior.never,
+      suffixIcon: _buildSuffixIcon(),
+    );
+  }
+
+  Widget _buildSuffixIcon() {
+    return AnimatedCrossFade(
+      duration: const Duration(milliseconds: 200),
+      firstChild: IconButton(
+        padding: EdgeInsets.zero,
+        icon: const Icon(
+          FontAwesomeIcons.xmark,
+          size: 20,
+          color: Color(0xFFA3A3A3),
+        ),
+        onPressed: _clearSearchField,
+      ),
+      secondChild: const SizedBox(),
+      crossFadeState: _searchController.text.isNotEmpty
+          ? CrossFadeState.showFirst
+          : CrossFadeState.showSecond,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: _buildContainerDecoration(),
+      padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 5),
       child: Row(
         children: [
           const Icon(
@@ -225,10 +300,17 @@ class _SearchMapState extends State<SearchMap> {
     );
   }
 
-  @override
-  void dispose() {
-    _searchController.dispose();
-    _debounce?.cancel();
-    super.dispose();
+  BoxDecoration _buildContainerDecoration() {
+    return BoxDecoration(
+      color: Colors.white,
+      borderRadius: BorderRadius.circular(10),
+      boxShadow: [
+        BoxShadow(
+          color: Colors.grey.withOpacity(0.25),
+          blurRadius: 5,
+          offset: const Offset(0, 2),
+        ),
+      ],
+    );
   }
 }
